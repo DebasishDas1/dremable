c82c7664546a2166d57819382ae26f8d07366898
Debasisg Das <iamdebasishno1@gmail.com>
2024-01-30 18:13:00 +0530

    BlogPost update

--- src/components/sheared/BlogPost.tsx
+++ src/components/sheared/BlogPost.tsx
@@ -1,27 +1,28 @@
-'use client'
-
-import Image from "next/image"
-import { formatDateTime } from '@/lib/utils'
-import Link from 'next/link'
+import Image from "next/image";
+import { formatDateTime } from '@/lib/utils';
+import Link from 'next/link';
 
 type BlogPostProps = {
-    title: string,
-    description: string | undefined,
-    id: string,
+    title: string;
+    description?: string;
+    id: string;
     date: Date;
     image: string;
 }
 
 const BlogPost = ({ title, description, id, date, image }: BlogPostProps) => {
+    const BlogPostTitle = title.split(' ').slice(0, 6).join(' ') + '...';
+    const BlogPostDescription = description ? description.split(' ').slice(0, 30).join(' ') + ' ...' : "";
+
     return (
         <div className="rounded-xl bg-white md:w-[600px] w-full p-4 shadow-lg">
-            <Link href={`/blog/${id}`} >
-                <h2 className="text-2xl font-semibold mb-2">{title}</h2>
+            <Link href={`/blog/${id}`}>
+                <h2 className="text-2xl font-semibold mb-2">{BlogPostTitle}</h2>
             </Link>
             <div className='flex flex-row '>
                 {description && (
                     <div className="text-sm my-2 basis-3/4">
-                        <div dangerouslySetInnerHTML={{ __html: description }} />
+                        <div dangerouslySetInnerHTML={{ __html: BlogPostDescription }} />
                     </div>
                 )}
                 {image && (
@@ -38,7 +39,7 @@ const BlogPost = ({ title, description, id, date, image }: BlogPostProps) => {
             </div>
             <h6 className="text-sm my-1 text-stone-500">{formatDateTime(date).dateOnly}</h6>
         </div>
-    )
+    );
 }
 
-export default BlogPost
+export default BlogPost;
